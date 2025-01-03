5e6721357909a15ee4433a50db464443cf3a8fae
Manu <formycodingwork@gmail.com>
2025-01-03 13:52:26 +0530

    Username length calculation script

---
+++ python/100-days-of-code/day-1/variables.py
@@ -0,0 +1,4 @@
+username = input("What is your name? ") # this will ask the user for their name
+length = len(username) # this will get the length of the username
+print(length) # this will print the length of the username
+
