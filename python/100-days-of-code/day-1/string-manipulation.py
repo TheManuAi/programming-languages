e4bc807232ad363bee1c4e40f69c9e64dda97f2c
Manu <formycodingwork@gmail.com>
2025-01-03 13:34:06 +0530

    String manipulation examples to demonstrate print formatting

---
+++ python/100-days-of-code/day-1/string-manipulation.py
@@ -0,0 +1,8 @@
+print("Pause 1. Use of \\n to adding another line of hello world")
+print("Hello world!\nHello world!\nHello world!")
+
+# Just for format
+print("\n")
+
+print("Pause 2. Adding a space between the strings")
+print("Hello" + " " + "Angela")
