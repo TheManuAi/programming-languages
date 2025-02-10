d5841f78cdef4b437e6b46fa014f721766148a94
Manu <formycodingwork@gmail.com>
2025-02-10 13:03:11 +0530

    Add function with multiple parameters implementation

---
+++ python/100-days-of-code/day-8/function_with_multiple_input.py
@@ -0,0 +1,7 @@
+def greet(name, location):
+    print(f"Hello, {name}!")
+    print(f"How's the weather at {location}?")
+
+n = input("What is your name: ")
+l = input("Where are you live: ")
+greet(n, l)
