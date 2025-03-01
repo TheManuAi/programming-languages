827f38f951c69e4adc95d3e198d04ff62800d113
Manu <formycodingwork@gmail.com>
2025-03-01 15:46:10 +0530

    Add try_except.py to convert temperature from Celsius to Fahrenheit with error handling

---
+++ python/Py4e(Book)/Chapter_3/try_except.py
@@ -0,0 +1,6 @@
+try:
+    cel = float(input("Enter the temperature in Celsius: "))   
+    feh = 1.8 * cel + 32
+    print(f"The temperature in Fehrenheit is: {feh}")
+except:
+    print("Please enter a number!")
