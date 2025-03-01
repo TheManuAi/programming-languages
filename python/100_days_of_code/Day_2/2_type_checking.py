0ae1bea4a982f6ca1370fbb70e5252d8a00e4db7
Manu <formycodingwork@gmail.com>
2025-03-01 16:18:44 +0530

    Add Day 2 scripts for subscripting, type checking, and a tip calculator

---
+++ python/100_days_of_code/Day_2/2_type_checking.py
@@ -0,0 +1,10 @@
+# Type Checking
+print(type("This is a normal text"))
+print(type(12345))
+print(type(3.14159))
+print(type(True))
+
+print("\n")
+
+# Type conversion
+print("Number of letters in your name is: " + str(len(input("Enter your name: "))))
