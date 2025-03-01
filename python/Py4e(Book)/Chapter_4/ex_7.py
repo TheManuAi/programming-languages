7e7606a0bcc8af64a36f872c4fbbabb27a78eaf8
Manu <formycodingwork@gmail.com>
2025-03-01 16:10:34 +0530

    Add computegrade function for score evaluation and input handling

---
+++ python/Py4e(Book)/Chapter_4/ex_7.py
@@ -0,0 +1,19 @@
+def computegrade(score):
+    if score > 100 or score < 0:
+        print("Score is not valid !")
+    elif score <= 100 and score >= 90:
+        print("A")
+    elif score >= 80:
+        print("B")
+    elif score >= 70:
+        print("C")
+    elif score >= 60:
+        print("D")
+    elif score < 60:
+        print("F")
+
+try:
+    score = float(input("Enter score (0-100): "))
+    computegrade(score)
+except:
+    print("Please enter a numaric value (0-100)")
