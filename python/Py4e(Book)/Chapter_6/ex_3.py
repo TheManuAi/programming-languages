51e7737de33add323d7c50d5902fc01c1a3377a1
Manu <formycodingwork@gmail.com>
2025-03-08 06:33:20 +0530

    Add program to count occurrences of a character in a user-input word

---
+++ python/Py4e(Book)/Chapter_6/ex_3.py
@@ -0,0 +1,13 @@
+def count(word, letter):
+    occurance = 0
+    for let in word:
+        if let == letter:
+            occurance += 1
+    print(f"The {letter} comes {occurance} times")
+    
+
+word = input("Enter the word: ")
+letter = input("Enter the charachter whose occurance you are intrested in: ")
+
+count(word=word, letter=letter)
+w
