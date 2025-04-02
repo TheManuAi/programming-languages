427d9b2df555aece666ae9cac6a8b3e6caf51ad5
Manu <formycodingwork@gmail.com>
2025-04-02 21:36:19 +0530

    Add program to perform multi-step arithmetic operations with error handling

---
+++ 300+ Python exercises/Simple Task/Assignments/Assignment_5.py
@@ -0,0 +1,52 @@
+try:
+    num1 = float(input("Enter the first number: "))
+    operator = input("Enter the operation(+, -, *, /):").strip()
+    num2 = float(input("Enter the second number: "))
+    operator2 = input("Enter the operation(+, -, *, /):").strip()
+    num3 = float(input("Enter the third number: "))
+
+    if operator == "/" and num2 == 0:
+        print("Division with 0 is not defined")
+    elif operator2 == "/" and num3 == 0:
+        print("Division with 0 is not defined")
+        
+    elif operator == "/" and operator2 == "/":
+        print(f"The result is {(num1/num2)/num3}")
+    elif operator == "/" and operator2 == "*":
+        print(f"The result is {(num1/num2)*num3}")
+    elif operator == "/" and operator2 == "+":
+        print(f"The result is {(num1/num2)+num3}")
+    elif operator == "/" and operator2 == "-":
+        print(f"The result is {(num1/num2)-num3}")
+
+    elif operator == "*" and operator2 == "/":
+        print(f"The result is {(num1*num2)/num3}")
+    elif operator == "*" and operator2 == "*":
+        print(f"The result is {(num1*num2)*num3}")
+    elif operator == "*" and operator2 == "+":
+        print(f"The result is {(num1*num2)+num3}")
+    elif operator == "*" and operator2 == "-":
+        print(f"The result is {(num1*num2)-num3}")
+
+    elif operator == "+" and operator2 == "/":
+        print(f"The result is {(num1+num2)/num3}")
+    elif operator == "+" and operator2 == "*":
+        print(f"The result is {(num1+num2)*num3}")
+    elif operator == "+" and operator2 == "+":
+        print(f"The result is {(num1+num2)+num3}")
+    elif operator == "+" and operator2 == "-":
+        print(f"The result is {(num1+num2)-num3}")
+
+    elif operator == "-" and operator2 == "/":
+        print(f"The result is {(num1-num2)/num3}")
+    elif operator == "-" and operator2 == "*":
+        print(f"The result is {(num1-num2)*num3}")
+    elif operator == "-" and operator2 == "+":
+        print(f"The result is {(num1-num2)+num3}")
+    elif operator == "-" and operator2 == "-":
+        print(f"The result is {(num1-num2)-num3}")
+
+    else:
+        print("Enter a valid operation")
+except ValueError:
+    print("Enter valid input(s)")
